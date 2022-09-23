205418a30421ce46942a9e9327651fbadf8b7485
tgold83 <ted.jonathan.gold@gmail.com>
2022-09-23 16:38:44 -0700

    BUGFIX functionality for adding to order in Program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -37,20 +37,25 @@ namespace CarbOrder
       Console.WriteLine("Your total is $" + orderTotal);
       
       Console.WriteLine("would you like to add to your order? Enter yes or no:");
-      if (Console.ReadLine() == "yes" || Console.ReadLine() == "Yes")
+      string addAnswer = Console.ReadLine();
+      switch (addAnswer)
       {
-        Console.WriteLine("How many loaves of bread would you like to add to your order? Enter a number:");
-        int addBread = Int32.Parse(Console.ReadLine());
-        Bread addBreadOrder = new Bread(addBread);
-        Console.WriteLine("How many pastries would you liketo add to your order? Enter a number:");
-        int addPastry = Int32.Parse(Console.ReadLine());
-        Pastry addPastryOrder = new Pastry(addPastry);
-        int NewOrderTotal = ((addBreadOrder.Discount() + addPastryOrder.Discount()) + orderTotal);
-        Console.WriteLine("Your new total is $" + NewOrderTotal);
-      }
-      else
-      {
-        Console.WriteLine("Your total is $" + orderTotal);
+        case "yes":
+          Console.WriteLine("How many loaves of bread would you like to add to your order? Enter a number:");
+          int addBread = Int32.Parse(Console.ReadLine());
+          Bread addBreadOrder = new Bread(addBread);
+          Console.WriteLine("How many pastries would you liketo add to your order? Enter a number:");
+          int addPastry = Int32.Parse(Console.ReadLine());
+          Pastry addPastryOrder = new Pastry(addPastry);
+          int NewOrderTotal = ((addBreadOrder.Discount() + addPastryOrder.Discount()) + orderTotal);
+          Console.WriteLine("Your new total is $" + NewOrderTotal);
+          break;
+        case "no":
+          Console.WriteLine("Your total is $" + orderTotal);
+          break;
+        default:
+          Console.WriteLine("Your total is $" + orderTotal);
+          break;
       }
     }
   }
